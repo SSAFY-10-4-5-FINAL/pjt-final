3abcd1210253064628422aaf6a099b58c6061dad
김영후 <eagle7449@naver.com>
2023-11-18 15:22:48 +0900

    nav bar router 이름 수정

--- client/whereismyhome_client/src/router/index.js
+++ client/whereismyhome_client/src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
-import layouts from "./layouts";
+import nav from "./layouts/nav";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [...layouts],
+  routes: [...nav],
 });
