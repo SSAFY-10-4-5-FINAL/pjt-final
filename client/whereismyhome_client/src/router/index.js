fb4cb3cdd8b3959b44780b5c1c9ca18d587971f2
김영후 <eagle7449@naver.com>
2023-11-18 17:10:40 +0900

    apart router 정의

--- client/whereismyhome_client/src/router/index.js
+++ client/whereismyhome_client/src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import navRoutes from "@/router/layouts/nav";
+import apart from "@/router/apart/apart";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // 각 router 파일에서 배열로 내보낸 routes들을 ...을 통해 iterable로 인식, 여기에서 읽어들이는 형태
-  routes: [...navRoutes],
+  routes: [...navRoutes, ...apart],
 });
 
 export default router;
