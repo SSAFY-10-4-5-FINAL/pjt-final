26612b8a3310fee3f6cfd37e190e4b0f6d983e72
eagle7449 <eagle7449@naver.com>
2023-11-20 15:05:33 +0900

    메인화면 center 바 ui 완료

--- client/whereismyhome_client/src/components/main/MainCenter.vue
+++ client/whereismyhome_client/src/components/main/MainCenter.vue
@@ -1,8 +1,11 @@
-<script setup></script>
+<script setup>
+import MainCenterSearch from "./MainCenterSearch.vue";
+</script>
 <template>
   <div class="main-center-wrap">
     <div class="ask-wrap">
       <div class="ask-text">어떤 집을 찾고 계세요?</div>
+      <div class="search-wrap"><MainCenterSearch /></div>
     </div>
   </div>
 </template>
@@ -11,20 +14,26 @@
 @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap");
 
 .main-center-wrap {
+  display: flex;
+  justify-content: center; /* 수평 가운데 정렬 */
   width: 100%;
   height: 550px;
   /* background-image: url(../../assets/img/bg_test_2.jpg); */
   background-color: grey;
-  padding: 120px;
 }
 
 .ask-wrap {
-  padding-top: 10px;
+  margin-top: 150px;
 }
+
 .ask-text {
   font-family: "Noto Sans KR", sans-serif;
   font-size: 48px;
   text-align: center;
   color: white;
+}
+
+.search-wrap {
+  width: 600px;
 }
 </style>
--- client/whereismyhome_client/src/components/main/MainCenterSearch.vue
+++ client/whereismyhome_client/src/components/main/MainCenterSearch.vue
@@ -1,5 +1,88 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="search-input-liner">
+    <div class="input-wrap">
+      <input type="text" placeholder="지역, 아파트 검색" />
+    </div>
+    <div class="btn-search-wrap"><button class="btn-search"></button></div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+/* input part */
+.search-input-liner {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-size-adjust: none;
+  -webkit-box-direction: normal;
+  margin-bottom: 10px;
+  padding: 0;
+  font: inherit;
+  border-radius: 4px;
+  overflow: hidden;
+  height: 50px;
+  vertical-align: top;
+  box-sizing: border-box;
+  position: relative;
+  background-color: white;
+  top: 10px;
+}
+.input-wrap {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-size-adjust: none;
+  -webkit-box-direction: normal;
+  margin: 0;
+  border: 0;
+  font: inherit;
+  vertical-align: baseline;
+  float: left;
+  padding: 0px 5px;
+  position: relative;
+  overflow: hidden;
+
+  width: 80%;
+}
+input {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-size-adjust: none;
+  -webkit-box-direction: normal;
+  width: 100%;
+  height: 50px;
+  border: none;
+  appearance: none;
+  outline: none;
+  box-shadow: none;
+  font-size: 13px;
+}
+
+.btn-search-wrap {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-size-adjust: none;
+  -webkit-box-direction: normal;
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font: inherit;
+  vertical-align: baseline;
+  float: right;
+  background-color: #708090;
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+}
+.btn-search {
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  text-size-adjust: none;
+  -webkit-box-direction: normal;
+  font-family: "SpoqaHanSans";
+  padding: 0px;
+  border: 0px;
+  margin: 0px;
+  vertical-align: middle;
+  cursor: pointer;
+  width: 50px;
+  height: 50px;
+  background: url("https://s.zigbang.com/www/images/ic_title_searchbar_30x30_nor_white.png") center
+    center / 100% no-repeat transparent;
+}
+</style>
