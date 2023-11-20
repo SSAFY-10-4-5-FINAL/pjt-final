6c7916b2843d6c080a6cee03c29a406eaa4ff205
eagle7449 <eagle7449@naver.com>
2023-11-20 17:40:34 +0900

    게시판 리스트

--- client/whereismyhome_client/src/router/layouts/nav.js
+++ client/whereismyhome_client/src/router/layouts/nav.js
@@ -13,4 +13,9 @@ export default [
     redirect: "/apart/right/under", // 아파트 페이지의 첫 페이지는 아파트 추천목록임
     children: ApartRight,
   },
+  {
+    path: "/community",
+    name: "CommunityView",
+    component: () => import("@/views/CommunityView.vue"),
+  },
 ];
---
+++ client/whereismyhome_client/src/views/CommunityView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import CommunityContent from "../components/community/CommunityContent.vue";
+import TheFooter from "@/components/layouts/TheFooter.vue";
+</script>
+<template>
+  <CommunityContent />
+  <TheFooter />
+</template>
+<style scoped></style>
