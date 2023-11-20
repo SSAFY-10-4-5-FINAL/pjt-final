000597bcbd9d7d00d4cca9ce6d763aa1f997966e
eagle7449 <eagle7449@naver.com>
2023-11-20 09:30:18 +0900

    검색 리스트

--- client/whereismyhome_client/src/components/apart/ApartRightView.vue
+++ client/whereismyhome_client/src/components/apart/ApartRightView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from "vue";
 import ApartRightUnder from "./right-items/ApartRightUnder.vue";
+import ApartSearchList from "./right-items/search_list/ApartSearchList.vue";
 
 // Data
 const inputText = ref("");
@@ -10,24 +11,18 @@ const inputText = ref("");
   <div class="container-fluid" id="right-bar">
     <div class="search-select-wrap item">
       <div id="toggle-btn-wrap">
-        <span class="toggle-text">아파트</span
-        ><button id="toggle-btn">toggle</button
+        <span class="toggle-text">아파트</span><button id="toggle-btn">toggle</button
         ><span class="toggle-text">지역</span>
       </div>
       <div class="search-input-liner">
         <div class="input-wrap">
-          <input
-            type="text"
-            placeholder="지역, 아파트 검색"
-            v-model="inputText"
-          />
+          <input type="text" placeholder="지역, 아파트 검색" v-model="inputText" />
         </div>
         <div class="btn-search-wrap"><button class="btn-search"></button></div>
       </div>
       <div id="star-link">
         <span>빠른 검색</span><br />
-        <RouterLink :to="{ name: 'ApartList' }"
-          ><a href="#">관심 아파트</a></RouterLink
+        <RouterLink :to="{ name: 'ApartList' }"><a href="#">관심 아파트</a></RouterLink
         ><span> | </span>
         <RouterLink :to="{ name: 'ApartDetail' }">
           <a href="#">관심 지역</a>
@@ -37,6 +32,7 @@ const inputText = ref("");
     <div class="list-detail-wrap item">
       <ApartRightUnder />
     </div>
+    <div v-show="inputText !== ''" id="apart-search-list-wrap"><ApartSearchList /></div>
   </div>
 </template>
 
---
+++ client/whereismyhome_client/src/components/apart/right-items/search_list/ApartSearchItem.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <div class="search-item">ㅎㅇㅎㅇ</div>
+</template>
+
+<style scoped>
+.search-item {
+  border: 1px solid #708090;
+  background-color: white;
+}
+</style>
