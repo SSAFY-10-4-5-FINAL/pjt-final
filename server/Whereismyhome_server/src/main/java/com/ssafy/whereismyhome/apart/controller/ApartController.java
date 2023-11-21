7cc64130a66b669ed47226820d17f0b710630dce
rkddofl000 <rkddofl000@naver.com>
2023-11-21 17:19:06 +0900

    responseentity 로 수정

--- server/Whereismyhome_server/src/main/java/com/ssafy/whereismyhome/apart/controller/ApartController.java
+++ server/Whereismyhome_server/src/main/java/com/ssafy/whereismyhome/apart/controller/ApartController.java
@@ -58,36 +58,36 @@ public class ApartController {
 
 	//동코드로 검색 - > 거기있는 아파트들 ApartDto list 로 리턴
 	@GetMapping("")
-	public List<ApartDto> getAparts(@RequestParam(required = true) String dongCode) throws SQLException {
+	public ResponseEntity getAparts(@RequestParam(required = true) String dongCode) throws SQLException {
 		List<ApartDto> apartDtoList = null;
 		apartDtoList = apartService.getApartByDongCode(dongCode);
 		//if (dong == null) apartDtoList = apartService.getAparts();
 		//else apartDtoList = apartService.getApartByDong(dong);
 		//System.out.println("in controller");
 		//System.out.println("end service call");
-		return apartDtoList;
+		return new ResponseEntity(apartDtoList,HttpStatus.OK);
 	}
 	
 	//아파트 이름 일부로 검색 -> 이름이 해당 문자열 포함하는 아파트 리스트 리턴.
 	@GetMapping("/searchname/{subName}")
-	public List<ApartDto> getApartBySubname(@PathVariable String subName) throws SQLException {
+	public ResponseEntity getApartBySubname(@PathVariable String subName) throws SQLException {
 		List<ApartDto> apartDtoList = null;
 		apartDtoList = apartService.getApartBySubname(subName);
-		return apartDtoList;
+		return new ResponseEntity(apartDtoList,HttpStatus.OK);
 	}
 	
 	//아파트 이름 일부로 검색 -> 이름이 해당 문자열 포함하는 아파트 리스트 리턴.
 	@GetMapping("/searchdong/{subDong}")
-	public List<DongDto> getDongBySubDong(@PathVariable String subDong) throws SQLException {
+	public ResponseEntity getDongBySubDong(@PathVariable String subDong) throws SQLException {
 		List<DongDto> DongDtoList = null;
 		DongDtoList = apartService.getDongBySubDong(subDong);
-		return DongDtoList;
+		return new ResponseEntity(DongDtoList,HttpStatus.OK);
 	}
 	
 	
 	//아파트코드로 검색 : 거래내역도 반환돼야함 -> 해당하는 아파트 객체 하나 ApartDealDto로 리턴/ 그안에 한 col 은 거래내역list 
 	@GetMapping("/{aptCode}")
-	public ApartDealDto getApartByCode(@PathVariable Long aptCode) throws SQLException {
+	public ResponseEntity getApartByCode(@PathVariable Long aptCode) throws SQLException {
 		System.out.println("in getAptbyCode");
 		ApartDealDto apartDealDto=new ApartDealDto();
 		ApartDto apartDto=null;
@@ -106,15 +106,15 @@ public class ApartController {
 		//apartDealDto.dealInfoList = dealInfoList;
 		//
 		System.out.println("end service call.-aptcode");
-		return apartDealDto;
+		return new ResponseEntity(apartDealDto,HttpStatus.OK);
 	}
 	
 	//조회수 순 아파트 GET(상위 10개)
 	@GetMapping("/aptlistbyhit")
-	public List<ApartDto> getApartByHit() throws SQLException {
+	public ResponseEntity getApartByHit() throws SQLException {
 		List<ApartDto> apartDtoList = null;
 		apartDtoList = apartService.getApartByHit();
-		return apartDtoList;
+		return new ResponseEntity(apartDtoList,HttpStatus.OK);
 	}
 	
 	
