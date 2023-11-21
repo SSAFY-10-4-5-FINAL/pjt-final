6b71c17ae7f84d4b7721c62501f75c91c9593722
rkddofl000 <rkddofl000@naver.com>
2023-11-21 17:23:37 +0900

    crossorigin수정

--- server/Whereismyhome_server/src/main/java/com/ssafy/whereismyhome/apart/controller/ApartController.java
+++ server/Whereismyhome_server/src/main/java/com/ssafy/whereismyhome/apart/controller/ApartController.java
@@ -5,11 +5,13 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +26,7 @@ import com.ssafy.whereismyhome.board.dto.BoardDto;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@CrossOrigin(origins = { "*" }, methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE} , maxAge = 6000)
 @RestController
 @RequestMapping("/apart")
 @AllArgsConstructor
